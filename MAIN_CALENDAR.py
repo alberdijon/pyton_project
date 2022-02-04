1719776afb5816b20bd0ef718f52bdfc3f0e12e8
alberdijon <alberdi.jon@uni.eus>
2022-02-04 12:12:10 +0100

    cambio

--- MAIN_CALENDAR.py
+++ MAIN_CALENDAR.py
@@ -11,29 +11,28 @@ while option!=11:
     print("1--Print calendar")
     print("2--Add event")
     print("3--Remove events")
-    print("4--Search if a day have events or not")
-    print("5--Show events")
-    print("6--Play Rock,Paper,Scissors")
-    print("7--Show how many events are added.")
-    print("8-Dawing machine.")
-    print("9--Exercises.")
-    print("10--Exit")
+    print("4--Show events")
+    print("5--Play Rock,Paper,Scissors")
+    print("6--Show how many events are added.")
+    print("7-Dawing machine.")
+    print("8--Exercises.")
+    print("9-Exit")
     option= int(input("Enter the number of an option from the menu: "))
     if option==1:
         print_calendar()
     if option==2:
         addevents(list_events)
-    if option == 8:
+    if option == 7:
         drawingMachine()
-    if option == 5:
+    if option == 4:
         show_events(list_events)
     if option == 3:
         deleteEventPosition(list_events)
-    if option == 7:
-        howManyEvents(list_events)
-    if option == 9:
-        exercises()
     if option == 6:
+        howManyEvents(list_events)
+    if option == 8:
+        exercises()
+    if option == 5:
         rock_paper_scisors()
 
 
